35a9818fa4a88f16508116f5242175663bce7370
André Coêlho <alcbcoelho@gmail.com>
2024-03-01 02:06:45 -0300

    Starting to implement Board and Piece classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
-        var pos = new Position(2, 3);
-
-        System.out.println(pos);
+        var board = new Board(8, 8);
     }
 }
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,18 @@
+package boardgame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+    public abstract boolean possibleMove(Position position);
+    public abstract boolean isThereAnyPossibleMove();
+}
