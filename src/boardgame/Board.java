dcce42c238d7d35affc5e20030e807dd8722b0e8
André Coêlho <alcbcoelho@gmail.com>
2024-03-04 03:12:43 -0300

    Implemented piece movement

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -35,6 +35,7 @@ public class Board {
 
         return pieces[position.getRow()][position.getColumn()];
     }
+
     public void placePiece(Piece piece, Position position) {
         if (thereIsAPiece(position))
             throw new BoardException("There is already a piece on position " + position);
@@ -42,7 +43,18 @@ public class Board {
         piece.position = position;  // this protected field from the Piece class is accessible here because Board is in the same package as Piece
     }
 
-//    public Piece removePiece(Position position)
+    public Piece removePiece(Position position) {
+        if (!positionExists(position))
+            throw new BoardException("Position not on the board");
+
+        if (piece(position) == null)
+            return null;
+
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
 
     private boolean positionExists(int row, int column) {
         return (row >= 0 && row < rows) && (column >= 0 && column < columns);
@@ -51,6 +63,7 @@ public class Board {
     public boolean positionExists(Position position) {
         return positionExists(position.getRow(), position.getColumn());
     }
+
     public boolean thereIsAPiece(Position position) {
         if (!positionExists(position))
             throw new BoardException("Position not on the board");
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,7 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -51,6 +52,26 @@ public class ChessMatch {
         placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 //    public boolean[][] possibleMoves(ChessPosition sourcePosition) {}
-//    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {}
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validateSourcePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+
+    private Piece makeMove(Position source, Position target) {
+        Piece p = board.removePiece(source);
+        Piece capturedPiece = board.removePiece(target);
+        board.placePiece(p, target);
+
+        return capturedPiece;
+    }
+
+    private void validateSourcePosition(Position position) {
+        if (!board.thereIsAPiece(position))
+            throw new ChessException("There is no piece on source position");
+    }
+
 //    public ChessPiece replacePromotedPiece(String type) {}
 }
