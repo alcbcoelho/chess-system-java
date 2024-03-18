5e53175f8494c0ff1ad30b43c4f7e91ac5c8f4ae
André Coêlho <alcbcoelho@gmail.com>
2024-03-17 22:05:37 -0300

    Implemented possible moves indicators

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -51,7 +51,12 @@ public class ChessMatch {
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
-//    public boolean[][] possibleMoves(ChessPosition sourcePosition) {}
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+        Position position = sourcePosition.toPosition();
+
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
